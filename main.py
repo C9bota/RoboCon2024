2fb08b88066d078feca5d69d7ba2e0440681b6d5
sodipot <11352215+sodipot@users.noreply.github.com>
2024-02-10 16:14:59 +0900

    Add basic multithreading components

---
+++ main.py
@@ -0,0 +1,43 @@
+import sys
+import threading
+import signal
+import time
+
+shared_flag = False
+lock = threading.Lock()
+
+def update_flag(l_lock):
+    global shared_flag
+    while True:
+        with lock:
+            shared_flag = True
+            print("update_flag(): Up Flag")
+
+        time.sleep(15)
+
+def check_flag(l_lock):
+    global shared_flag
+    while True:
+        with lock:
+            if shared_flag:
+                shared_flag = False
+                print("check_flag(): Down Flag")
+            else:
+                print("check_flag(): Flag is False")
+        
+        time.sleep(4)
+
+def exit_program(signal, frame):
+    print("Exiting...")
+    sys.exit(1)
+
+threads = []
+
+t1 = threading.Thread(target=update_flag, args = (lock,))
+threads.append(t1)
+t2 = threading.Thread(target=check_flag, args = (lock,))
+threads.append(t2)
+t1.start()
+t2.start()
+
+signal.signal(signal.SIGINT, exit_program)
